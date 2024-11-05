5d4dccfb7708b67b0a4f5612803ad9f47d4979c9
SteveMama <pkompally@gmail.com>
2024-11-05 16:31:01 -0500

    --added num_classes---

--- Diff_Transformer.py
+++ Diff_Transformer.py
@@ -1,29 +1,31 @@
 import torch
-
 from diff_layer import *
+
 
 class DifferentialTransformer(nn.Module):
     def __init__(self,
                  vocab_size,
-                 d_model = 3072,
-                 n_layers = 28,
-                 d_head = 128,
-                 n_heads = 12,
-                 max_seq_len = 4096,
-                 dropout = 0.1):
+                 num_classes=4,
+                 d_model=3072,
+                 n_layers=28,
+                 d_head=128,
+                 n_heads=12,
+                 max_seq_len=4096,
+                 dropout=0.1):
         super().__init__()
         self.token_embedding = nn.Embedding(vocab_size, d_model)
         self.position_embedding = nn.Embedding(max_seq_len, d_model)
 
-        self.layers = nn.ModuleList([DifferentialTransformerLayer(d_model, d_head, n_heads, dropout)
-                                     for _ in range(n_layers)])
+        self.layers = nn.ModuleList([
+            DifferentialTransformerLayer(d_model, d_head, n_heads, dropout)
+            for _ in range(n_layers)
+        ])
 
         self.norm = nn.LayerNorm(d_model, elementwise_affine=False)
-        self.head = nn.Linear(d_model, vocab_size, bias = False)
+        self.head = nn.Linear(d_model, num_classes, bias=False)
 
-    def forward(self, x, mask = None):
+    def forward(self, x, attention_mask=None):
         b, t = x.size()
-
         positions = torch.arange(t, device=x.device).unsqueeze(0).expand(b, -1)
 
         token_embed = self.token_embedding(x)
@@ -31,9 +33,9 @@ class DifferentialTransformer(nn.Module):
         x = token_embed + pos_embed
 
         for layer in self.layers:
-            x = layer(x, mask)
+            x = layer(x, mask=attention_mask)
 
         x = self.norm(x)
+        x = x.mean(dim=1)
         x = self.head(x)
-
         return x
